81a9fbfb651a88c3b255b55d73df8a38b84be5f9
Axel Padilla <adpadillar25@gmail.com>
2021-06-14 12:51:05 -0600

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./pages/**/", "./components/**/"],
+  darkMode: false, // or 'media' or 'class',
+  mode: "jit",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
