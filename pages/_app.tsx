22c2b3239a42691babf0872b032f84b837fa3ee0
Axel Padilla <adpadillar25@gmail.com>
2021-08-02 12:58:44 -0600

    Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,16 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KKJH19PBXZ"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          {/*@ts-ignore */}
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-KKJH19PBXZ');
+        </script>
       </Head>
       <Component {...pageProps} />
     </>
